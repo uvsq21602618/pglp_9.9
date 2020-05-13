a7acda895b36c87a17d2e0d8b719884727ead853
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-05-13 18:33:55 +0200

    Creation de composantDessin

---
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_9_9/ComposantDessin.java
@@ -0,0 +1,39 @@
+package fr.uvsq.uvsq21602618.pglp_9_9;
+
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
+/**
+ * Le composite derive de Forme.
+ * @author Nathalie
+ *
+ */
+public class ComposantDessin {
+    /**
+     * Liste de composants. 
+     */
+    private ArrayList<Dessin> dessinFils = new ArrayList<Dessin>();
+    /**
+     * Methode pour afficher les informations du composant.
+     */
+    public void affichage() {
+        for (Dessin dessin : dessinFils) {
+            dessin.affiche();
+        }
+    }
+    /**
+     * Ajouter un dessin a la liste.
+     * @param dessin un dessin
+     */
+    public void ajoute(Dessin dessin) {
+        dessinFils.add(dessin);
+    }
+    /**
+     * Retirer un dessin a la liste.
+     * @param dessin un dessin
+     */
+    public void retire(Dessin dessin) {
+        dessinFils.remove(dessin);
+    }
+}
