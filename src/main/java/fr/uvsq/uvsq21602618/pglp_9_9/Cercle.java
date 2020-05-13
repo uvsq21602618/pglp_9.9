27f21eca60ff52f8cf948fd553fd0625b8cd41ed
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-05-13 15:36:58 +0200

    Constructeur Cercle

--- src/main/java/fr/uvsq/uvsq21602618/pglp_9_9/Cercle.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_9_9/Cercle.java
@@ -5,7 +5,20 @@ package fr.uvsq.uvsq21602618.pglp_9_9;
  *
  */
 public class Cercle extends Forme {
-
+    /**
+     * Le centre du cercle.
+     */
+    private Point centre;
+    /**
+     * Le rayon du cercle.
+     */
+    private int rayon;
+    
+    public Cercle (final String nom2, final Point p, final int ray) {
+        this.nom = nom2;
+        this.centre = p;
+        this.rayon = ray;
+    }
     @Override
     public void dessine() {
         // TODO Auto-generated method stub
